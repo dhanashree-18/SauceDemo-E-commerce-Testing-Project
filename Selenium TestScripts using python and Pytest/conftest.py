05fa8878ac2e459afd2e296f8cc5c9377da779d7
dhanashree-18 <dhokardhanashree@gmail.com>
2026-06-10 14:01:59 +0530

    Add headless mode to conftest.py

--- Selenium TestScripts using python and Pytest/conftest.py
+++ Selenium TestScripts using python and Pytest/conftest.py
@@ -1,9 +1,16 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def setup():
-	driver = webdriver.Chrome()
+	chrome_options = Options()
+	chrome_options.add_argument("--headless")
+	chrome_options.add_argument("--no-sandbox")
+	chrome_options.add_argument("--disable-dev-shm-usage")
+	chrome_options.add_argument("--window-size=1920,1080")
+
+	driver = webdriver.Chrome(options=chrome_options)
 	driver.maximize_window()
 	driver.get("https://www.saucedemo.com/")
 	driver.implicitly_wait(10)

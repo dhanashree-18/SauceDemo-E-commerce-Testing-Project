3fe9c35f06b3917f76d56a6426cfcdb517582423
dhanashree-18 <dhokardhanashree@gmail.com>
2026-06-30 13:10:52 +0530

    Fixed headless mode in conftest.py

--- Selenium TestScripts using python and Pytest/tests/conftest.py
+++ Selenium TestScripts using python and Pytest/tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import os
 
 @pytest.fixture
 def setup():
@@ -11,10 +12,12 @@ def setup():
 		"profile.password_manager_leak_detection": False
 	})
 
-	chrome_options.add_argument("--headless")
-	chrome_options.add_argument("--no-sandbox")
-	chrome_options.add_argument("--disable-dev-shm-usage")
-	chrome_options.add_argument("--window-size=1920,1080")
+	if os.environ.get("CI"):
+
+		chrome_options.add_argument("--headless")
+		chrome_options.add_argument("--no-sandbox")
+		chrome_options.add_argument("--disable-dev-shm-usage")
+		chrome_options.add_argument("--window-size=1920,1080")
 	
 	driver = webdriver.Chrome(options=chrome_options)
 	driver.maximize_window()
